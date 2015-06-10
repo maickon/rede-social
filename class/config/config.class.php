2ac79716f320f5caa094c7c0d67abbdf83a6e327
Maickon Rangel <maickon4developers@gmail.com>
2015-06-10 17:26:41 -0300

    novas configuracoes

--- class/config/config.class.php
+++ class/config/config.class.php
@@ -10,10 +10,14 @@ class Config{
 		$this->css_path				= "css/";
 		$this->css_bootstrap 		= "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css";
 		$this->css_bootstrap_min 	= "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css";
+		$this->css_justified_nav 	= "http://getbootstrap.com/examples/justified-nav/justified-nav.css";
+		
 		//js path
 		$this->js_path				= "js/";
 		$this->js_bootstrap 		= "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js";
 		$this->js_jquery 			= "https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js";
+		$this->js_ 					= "http://getbootstrap.com/assets/js/docs.min.js";
+		//classes
 		$this->class_path			= "class/";
 		
 		//caminhos especifico
