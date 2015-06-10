397ab68eb6b46cdde7bb8f64541f59c1e1f6e910
Maickon Rangel <maickon4developers@gmail.com>
2015-06-10 09:38:07 -0300

    adicionando funcoes de get_header e get_footer a pagina inicial

--- index.php
+++ index.php
@@ -4,21 +4,8 @@ require_once 'init.php';
 
 $config = new Config();
 $tag = new Tags();
-echo 
-$tag->imprime('<!DOCTYPE html>');
-$tag->html();
-	$tag->head();
-		$tag->title();
-			$tag->imprime('Rede Social Title');
-		$tag->title;
-		
-		$tag->link('rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"');
-		$tag->link(' rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css"');
-		$tag->link(' rel="stylesheet" href="'.$config->css_path_index.'"');
-		$tag->script('src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"');
-		$tag->script;
-		
-	$tag->head;
+
+	require_once get_header();
 	
 	$tag->body();
 		global $labels;
@@ -46,9 +33,9 @@ $tag->html();
 		
 		new Rotas(isset($_GET['p'])?$_GET['p']:'home');
 		
-		new FooterBar($labels, $links, "Maickon Rangel", "2015");
+		require_once get_footer();
 		
-		$tag->script('src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"');
+		$tag->script('src="'.$config->js_jquery.'"');
 		$tag->script;
 	$tag->body;
 $tag->html;
