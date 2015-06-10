b29118a7e9d673c682d5cf0eb45093d18d744db0
Maickon Rangel <maickon4developers@gmail.com>
2015-06-10 09:36:55 -0300

    novo arquivo reservado para novas funcoes no sistema

--- funcoes/funcoes.php
+++ funcoes/funcoes.php
@@ -1,4 +1,24 @@
 <?php
+$header_path = dirname(dirname(__FILE__));
+
 function login_user($email, $senha){
-	$_SESSION['email'] = 
+	$_SESSION['email'] =  '';
+}
+
+/*
+ * get_header()
+ * retorna o caminho do cabeçalho
+ */
+function get_header(){
+	global $header_path;
+	return $header_path.'/header.php';
+}
+
+/*
+ * get_footer()
+*  retorna o caminho do rodape
+*/
+function get_footer(){
+	global $header_path;
+	return $header_path.'/footer.php';
 }
