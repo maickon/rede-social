2516e002b2a8fd2ef9080ad0217645f8bf39b85f
Maickon Rangel <maickon4developers@gmail.com>
2015-06-10 09:40:07 -0300

    criado o arquivo de cabecalho pra ser chamado na index.php

---
+++ header.php
@@ -0,0 +1,16 @@
+<?php
+global $tag;
+$tag->imprime('<!DOCTYPE html>');
+$tag->html();
+	$tag->head();
+		$tag->title();
+			$tag->imprime('Rede Social Title');
+		$tag->title;
+		
+		$tag->link('rel="stylesheet" href="'.$config->css_bootstrap.'"');
+		$tag->link(' rel="stylesheet" href="'.$config->css_bootstrap_min.'"');
+		$tag->link(' rel="stylesheet" href="'.$config->css_path_index.'"');
+		$tag->script('src="'.$config->js_bootstrap.'"');
+		$tag->script;
+		
+	$tag->head;
