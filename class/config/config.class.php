4caa5be801ea03b2fa258f61215f166c5f1aa100
Maickon Rangel <maickon4developers@gmail.com>
2015-06-10 09:34:56 -0300

    adicionado novos atributos de configuracao

--- class/config/config.class.php
+++ class/config/config.class.php
@@ -6,8 +6,14 @@ class Config{
 	function __construct(){
 		$this->site_name 			= "Rede Social";
 		$this->base_url		 		= "http://127.0.0.1/rede_social/";
+		//css path
 		$this->css_path				= "css/";
+		$this->css_bootstrap 		= "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css";
+		$this->css_bootstrap_min 	= "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css";
+		//js path
 		$this->js_path				= "js/";
+		$this->js_bootstrap 		= "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js";
+		$this->js_jquery 			= "https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js";
 		$this->class_path			= "class/";
 		
 		//caminhos especifico
