f1ed3e024f9bf9fcba46fe8719921e20da6cb4ec
Maickon Rangel <maickon4developers@gmail.com>
2015-06-11 10:29:03 -0300

    Adicionado funcao bild_link_header() a pagina de cadastro de usuario

--- form/usuario/create.php
+++ form/usuario/create.php
@@ -15,7 +15,7 @@ $db = new Db_crud();
 
 
 if($db->insere('usuario', $user->attr)):
-	header('Location:'.BASE_URL.'/index.php?success=true');
+	bild_lik_header(array('type'=>'error','msg'=>CADASTRO_ERROR),'index.php?');
 else:
-	header('Location:'.BASE_URL.'/index.php?success=false');
+	bild_lik_header(array('type'=>'success','msg'=>CADASTRO_SUCCESS),'index.php?');
 endif;
