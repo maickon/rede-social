4dbc8dee2c056cadd7827ca331dd17d151dca25a
Maickon Rangel <maickon4developers@gmail.com>
2015-06-11 15:58:28 -0300

    novas funcoes para a pagina de usuarios

--- form/usuario/funcoes.php
+++ form/usuario/funcoes.php
@@ -85,3 +85,69 @@ function get_feed_box($title){
 		$tag->div;
 	$tag->div;
 }
+
+function user_feed_show(){
+	global $tag;
+	$tag->div('id="user_status"');
+		$feeds = array('menssagens','músicas','videos','fotos');
+		foreach($feeds as $seletor):
+			$tag->a('href="#"');
+				$tag->span();
+					$tag->imprime($seletor);
+				$tag->span;
+			$tag->a;
+	
+			$tag->table('class="table table-bordered"');
+				$tag->tr();
+					$tag->th();
+						$tag->span('class="glyphicon glyphicon-arrow-right"');
+						$tag->span;
+					$tag->th;
+				
+					$tag->td();
+						$tag->imprime('Pontos');
+					$tag->td;
+					
+					$tag->td();
+						$tag->span('class="badge"');
+							$tag->imprime('100');
+						$tag->span;
+					$tag->td;
+					
+					$tag->td();
+						$tag->imprime('Ranking');
+					$tag->td;
+					
+					$tag->td();
+						$tag->span('class="badge"');
+							$tag->imprime('9876');
+						$tag->span;
+					$tag->td;
+				$tag->tr;
+			$tag->table;
+		endforeach;
+	$tag->div;
+}
+
+function buttons_post(){
+	global $tag;
+	$likes = array('?action=top' => 'Top', '?action=bom' => 'Bom', '?action=ruim' => 'Ruim', '?action=compartilhar' => 'Compartilhar');
+	foreach($likes as $key => $botton):
+		if(end($likes) == $botton):
+			$tag->li('role="presentation"');
+				$tag->a('href="'.$key.'"');
+					$tag->imprime($botton);
+				$tag->a;
+			$tag->li;
+		else:
+			$tag->li('role="presentation"');
+				$tag->a('href="'.$key.'"');
+					$tag->imprime($botton);
+					$tag->span('class="badge"');
+						$tag->imprime('93');
+					$tag->span;
+				$tag->a;
+			$tag->li;
+		endif;
+	endforeach;
+}
