600e29cf93ab556609d7357fd3428a42a02bf456
Maickon Rangel <maickon4developers@gmail.com>
2015-06-11 15:56:57 -0300

    Adicionado o login como variavel de sessao

--- form/login/login.php
+++ form/login/login.php
@@ -9,8 +9,10 @@ $user = new Usuario();
 $user->login = $_POST['cadastro']['login'];
 $user->senha = $_POST['cadastro']['senha'];
 
-if($db->buscar_usuario('usuario', $user->login, $user->senha)):
-	$_SESSION["user_ativo"] = $user->login;
+$user_db = $db->buscar_usuario('usuario', $user->login, $user->senha);
+if($user_db):
+	$_SESSION["user_ativo_login"] = $user_db->login;
+	$_SESSION["user_ativo_nome"] = $user_db->nome;
 	bild_lik_header(USER_BASE_URL, array('type'=>'success','msg'=>LOGIN_SUCCESS),'/index.php?');
 else:
 	bild_lik_header(array('type'=>'error','msg'=>LOGIN_ERRORS), 'index.php?');
